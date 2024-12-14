5baba5fcc21c8b36588bb555679e4037c616cd4c
Kazehaya-ctrl <mahitomaki96@gmail.com>
2024-12-15 00:14:58 +0530

    upt: post and get request in cpp using boost lib and for nlohmann for json formatting

--- server.cpp
+++ server.cpp
@@ -1,21 +1,42 @@
 #include<boost/asio.hpp>
-#include <nlohmann/json.hpp>
+#include<nlohmann/json.hpp>
 #include<iostream>
+#include<sstream>
 #include<string>
 
 using boost::asio::ip::tcp;
 using json = nlohmann::json;
 
-void send_request(tcp::socket& socket,const json& response_json) {
-	try {
-		std::string body = response_json.dump();
-		std::string response = 
-		"HTTP/1.1 200 OK\r\n" "Content-Type: application/json\r\n"  
+std::string response_format(const json& response_json) {
+	std::string body = response_json.dump();
+	std::string response = "HTTP/1.1 200 OK\r\n" "Content-Type: Application/json\r\n"  
 		"Content-Length: " + std::to_string(body.size()) + "\r\n\r\n" + body;
 
+	return response;
+}
+
+void get_request(tcp::socket& socket, const json& response_json) {
+	try {
+		std::string response = response_format(response_json);
 		boost::asio::write(socket, boost::asio::buffer(response));
 	} catch (const std::exception& e) {
 		std::cerr << "Error + " << e.what() << std::endl;
+	}
+}
+
+void post_request(tcp::socket& socket, const std::string& request) {
+	size_t start_json = request.find("\r\n\r\n");
+	try {
+		if(start_json != std::string::npos) {
+			std::string response = request.substr(start_json + 4);
+			json body = json::parse(response);
+			std::string whole_response = response_format(body);
+			boost::asio::write(socket, boost::asio::buffer(whole_response));
+		} else {
+			std::cerr << "Error: " << std::endl;
+		}
+	} catch (std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
 	}
 }
 
@@ -29,11 +50,20 @@ void request_handler(tcp::socket& socket) {
 		if(!error) {
 			json reponse = {{"msg", "Hello world"}};
 			request.append(buffer, length);
-			std::cout << "Request recieved " << request << std::endl;
-			send_request(socket, reponse);	
-			if(error) {
-				std::cerr << "Error: " << error << std::endl;
+
+			std::istringstream request_identifier(request);
+			std::string method;
+			request_identifier >> method;
+
+			if(method == "GET") {
+				std::cout << "GET Request: " << request << std::endl;
+				get_request(socket, reponse);	
 			}
+		
+			if(method == "POST") {
+				std::cout << "POST request: " << request << std::endl;
+				post_request(socket, request);
+ 			}
 		} else {
 			std::cerr << "Request can't be read: " << std::endl;
 		}
@@ -60,6 +90,6 @@ int main() {
 	} catch (const std::exception& e) {
 		std::cerr << "Error + " << e.what() << std::endl;
 	}
-	
+
 	return 0;
 }
